aa9606d7e3264ee4869a4c4373d964e6ba3824e5
tidunguyen <tidu.nguyen.2000@gmail.com>
2021-09-28 15:38:33 +0700

    reorganize scripts

---
+++ scripts/init
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+if [ -f .env ]; then
+  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+fi
--- scripts/minikube/util.sh
+++ scripts/minikube/util.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+if [ -f .env ]; then
+  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+fi
 
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
